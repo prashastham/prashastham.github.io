ad1e2a9736be2f2ebfd2d6970a59388fd0494ac5
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2022-04-13 23:58:59 +0530

    [main] Complete website without content and links

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ scene.add( gridHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 //Addding torus knot
-const geometry  = new THREE.TorusKnotGeometry(10, 3, 100, 64);
-const material = new THREE.MeshToonMaterial({color:0x80ff00});
+const geometry  = new THREE.TorusGeometry(10, 2.5, 16, 64);
+const material = new THREE.MeshToonMaterial({color:0x0cf78d});
 const torusKnot = new THREE.Mesh(geometry, material);
 scene.add(torusKnot);
 
@@ -49,7 +49,7 @@ function addMonkey(){
 
             object.position.set(x, y, z);
             object.rotation.set(x, y, z);
-            //object.scale.set(0.75, 0.75, 0.75);
+            object.scale.set(1.5, 1.5, 1.5);
             scene.add(object);
         }
         );
@@ -61,6 +61,44 @@ Array(100).fill().forEach(addMonkey);
 //Adding background
 const comicTexture = new THREE.TextureLoader().load('background.jpg');
 scene.background = comicTexture;
+
+//Adding avatar
+const avatarTexture = new THREE.TextureLoader().load('prashastha.jpg');
+const avatar = new THREE.Mesh(
+    new THREE.BoxGeometry(10, 10, 10),
+    new THREE.MeshBasicMaterial({map:avatarTexture})
+);
+scene.add(avatar);
+
+//Adding sphere
+const nebulaTexture = new THREE.TextureLoader().load('nebula.jpg');
+const nebula = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({map:nebulaTexture})
+);
+scene.add(nebula);
+
+
+nebula.position.z = 30;
+nebula.position.setX(-10);
+
+//Scrolling
+function moveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+    nebula.rotation.x += 0.05;
+    nebula.rotation.y += 0.075;
+    nebula.rotation.z += 0.05;
+
+    avatar.rotation.y += 0.01;
+    avatar.rotation.z += 0.01;
+
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+moveCamera();
 
 //Window resizer
 window.addEventListener( 'resize', onWindowResize );
@@ -80,8 +118,11 @@ function onWindowResize() {
 function animate(){
     requestAnimationFrame(animate);
 
-    torusKnot.rotation.x += 0.02;
-    torusKnot.rotation.y += 0.02;
+    torusKnot.rotation.x += 0.01;
+    torusKnot.rotation.y += 0.01;
+    torusKnot.rotation.z += 0.01;
+
+    nebula.rotation.x += 0.005;
 
     controls.update();
 
