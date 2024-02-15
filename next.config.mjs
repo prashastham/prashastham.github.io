0ff2fd05020e9aee46d2caf8a5c1139b815191a3
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2024-02-15 14:14:12 +0530

    Adding basepath to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	basePath: "",
 	output: "export",  // <=== enables static exports
   	reactStrictMode: true,
 };
